4c19e46236d0ea4a1816830328ab2ab11d4e54e7
Astralchroma <astralchroma@proton.me>
2024-08-26 02:01:58 +0100

    Replace Podman/Docker Containers with NixOS Containers

--- outpost.nix
+++ outpost.nix
@@ -14,6 +14,12 @@ in {
 			options = [ "compress=lzo" ];
 		};
 
+		"/srv" = {
+			device = "/dev/disk/by-uuid/0a2e0044-98e6-43c7-9900-d1885b1bd4d0";
+			fsType = "btrfs";
+			options = [ "subvol=/Server" ];
+		};
+
 		"/boot" = {
 			device = "/dev/disk/by-uuid/E625-43E4";
 			fsType = "vfat";
@@ -23,63 +29,138 @@ in {
 	networking = {
 		hostName = "outpost";
 		useDHCP = true;
+		firewall.allowedTCPPorts = [ 80 443 ];
 	};
 
-	services.tailscale.enable = true;
+	containers.mongo = {
+		autoStart = true;
 
-	users.users.emily.packages = [ pkgs.mongosh ];
-
-	virtualisation = {
-		containers.enable = true;
-
-		podman = {
-			enable = true;
-			dockerCompat = true;
-			defaultNetwork.settings.dns_enabled = true;
+		bindMounts = {
+			"/srv" = {
+				hostPath = "/srv/mongo";
+				isReadOnly = false;
+			};
 		};
 
-		oci-containers.containers = {
-			caddy = {
-				image = "caddy:alpine";
-				ports = [
-					"0.0.0.0:80:80"
-					"0.0.0.0:443:443"
-				];
-				volumes = [
-					"/srv/caddy/Caddyfile:/etc/caddy/Caddyfile"
-					"/srv/caddy/config/:/config/caddy/"
-					"/srv/caddy/data/:/data/caddy/"
-					"/srv/http/:/srv/"
-				];
-			};
+		config = {
+			nixpkgs.config.allowUnfree = true;
+			system.stateVersion = "24.11";
 
-			mongo = {
-				image = "mongo";
-				ports = [ "127.0.0.1:27017:27017" ];
-				environment = {
-					MONGO_INITDB_ROOT_USERNAME = "root";
-					MONGO_INITDB_ROOT_PASSWORD = values.mongo_password;
-				};
-				volumes = [ "/srv/mongodb:/data/db" ];
+			services.mongodb = {
+				enable = true;
+				dbpath = "/srv";
 			};
+		};
+	};
 
-			aggregator = {
-				image = "eclipse-temurin:17-jre";
+	containers.aggregator = {
+		autoStart = true;
+
+		bindMounts = {
+			"/srv" = {
+				hostPath = "/srv/aggregator";
+				isReadOnly = false;
+			};
+		};
+
+		config = {
+			system.stateVersion = "24.11";
+
+			environment.systemPackages = [ pkgs.jdk17 ];
+
+			systemd.services.aggregator = {
+				enable = true;
+				description = "Aggregator";
+				unitConfig.Type = "simple";
+				serviceConfig.ExecStart = "${pkgs.jdk17}/bin/java -jar /srv/build/libs/Aggregator-1.4.1-all.jar";
+				wantedBy = [ "multi-user.target" ];
 				environment = {
 					DISCORD_TOKEN = values.discord_token;
 					MONGO_URI = values.mongo_uri;
 					MONGO_DATABASE = "aggregator";
 					OWNER_SNOWFLAKE = "521031433972744193";
 				};
-				volumes = [ "/srv/aggregator:/srv" ];
-				cmd = [ "java" "-jar" "/srv/build/libs/Aggregator-1.4.1-all.jar" ];
-			};
-
-			headscale = {
-				image = "headscale/headscale:0.22.3";
-				volumes = [ "/srv/headscale:/etc/headscale" ];
-				cmd = [ "headscale" "serve" ];
 			};
 		};
 	};
+
+	containers.caddy = {
+		autoStart = true;
+
+		bindMounts = {
+			"/srv" = {
+				hostPath = "/srv/http";
+				isReadOnly = true;
+			};
+		};
+
+		config = {
+			system.stateVersion = "24.11";
+
+			services.caddy = {
+				enable = true;
+				email = "astralchroma@proton.me";
+
+				virtualHosts."https://astralchroma.dev".extraConfig = ''
+					@stripExtensions path_regexp strip (.*)\.(html)
+					redir @stripExtensions {re.strip.1} permanent
+
+					redir /index / permanent
+					header / Link "<fonts/exo_2-reduced.ttf>;rel=preload;as=font,<fonts/exo_2-italic-reduced.ttf>;rel=preload;as=font,<images/background.svg>;rel=reload;as=image,<images/avatar.png>;rel=preload;as=image,<images/github.svg>;rel=preload;as=image,<images/youtube.svg>;rel=preload;as=image,<images/mail.svg>;rel=preload;as=image,<images/code.svg>;rel=preload;as=image,<images/globe.svg>;rel=preload;as=image"
+
+					root * /srv/
+
+					file_server {
+						hide /srv/.git/
+					}
+
+					try_files {path} {path}/ {path}.html
+				'';
+
+				virtualHosts."https://www.astralchroma.dev".extraConfig = ''
+					redir https://astralchroma.dev{uri} permanent
+				'';
+
+				virtualHosts."https://tailscale.astralchroma.dev".extraConfig = ''
+					reverse_proxy http://localhost:8080
+				'';
+
+				virtualHosts."https://gateway.astralchroma.dev".extraConfig = ''
+					reverse_proxy http://100.64.0.1:8096
+				'';
+			};
+		};
+	};
+
+	containers.headscale = {
+		autoStart = true;
+
+		bindMounts = {
+			"/srv" = {
+				hostPath = "/srv/headscale";
+				isReadOnly = false;
+			};
+		};
+
+		config = {
+			system.stateVersion = "24.11";
+
+			services.headscale = {
+				enable = true;
+
+				settings = {
+					server_url = "https://tailscale.astralchroma.dev/";
+					db_path = "/srv/db.sqlite";
+					private_key_path = "/srv/private.key";
+					noise.private_key_path = "/srv/noise_private.key";
+				};
+			};
+
+			systemd.services.headscale.serviceConfig.ReadWritePaths = "/srv";
+		};
+	};
+
+	services.tailscale.enable = true;
+
+	users.users.emily.packages = [ pkgs.mongosh ];
 }
